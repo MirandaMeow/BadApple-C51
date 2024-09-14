3e4fc2d59f83d8594bf0dd69fec56a0e2736780d
MirandaMeow <444037570@qq.com>
2024-09-14 18:36:42 +0800

    first submit

---
+++ C51/main.c
@@ -0,0 +1,58 @@
+#include <STC89C5xRC.H>
+#include "LED88.h"
+
+#define uint unsigned int
+#define uchar unsigned char
+
+uint count;
+
+uchar Frame[32] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+uchar out[32] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+};
+
+// 初始化串口
+void Uart_Init() {  //9600bps@11.0592MHz
+    PCON &= 0x7F;       //波特率不倍速
+    SCON = 0x50;        //8位数据,可变波特率
+    AUXR &= 0xBF;       //定时器时钟12T模式
+    AUXR &= 0xFE;       //串口1选择定时器1为波特率发生器
+    TMOD &= 0x0F;       //设置定时器模式
+    TMOD |= 0x20;       //设置定时器模式
+    TL1 = 0xFD;         //设置定时初始值
+    TH1 = 0xFD;         //设置定时重载值
+    ET1 = 0;            //禁止定时器中断
+    TR1 = 1;            //定时器1开始计时
+    EA = 1;
+    ES = 1;
+}
+
+void UART_Routine() interrupt 4 {
+    uint i;
+    if (RI == 1) {
+        RI = 0;
+        Frame[count] = SBUF;
+        count++;
+        if (count >= 32) {
+            count = 0;
+            for (i = 0; i < 32; i++) {
+                out[i] = Frame[i];
+            }
+        }
+    }
+}
+
+void main() {
+    Uart_Init();
+    LED88_Init();
+
+    while (1) {
+        LED88_Animation(out, out, 0);
+    };
+}
+
